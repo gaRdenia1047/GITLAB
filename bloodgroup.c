5f27cff566fea05f6631d396355432f0b0aa2173
gaRdenia1047 <pinyo.patteera@gmail.com>
2022-09-23 00:55:09 +0700

    add and edit 2 place

--- bloodgroup.c
+++ bloodgroup.c
@@ -28,7 +28,7 @@ void main()
     }
     else if (strcmp(G, "B") ==0)
     {
-      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
+      printf("%s, Bethoven. Well, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
     else if (strcmp(G, "AB") == 0)
@@ -38,7 +38,7 @@ void main()
     }
     else
     {
-      printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
+      printf("%s, Oatmeal. Nice! You can give blood to all: O, A, B, AB\n", Name);
       printf("  But Sad! You can receive blood only from: O\n");
     }
     printf("\nContinue (YES for Yes)? ");
